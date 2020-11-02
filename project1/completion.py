462699a0a102a5b7f87c878853da27bb67fc7c24
swy20190 <K201410109@163.com>
2020-11-02 19:55:07 +0800

    some tool methods

---
+++ project1/completion.py
@@ -0,0 +1,15 @@
+import random
+import numpy as np
+
+
+def complete(img, size, mode="random"):
+    """
+    :param img: the src texture
+    :param size: size of result
+    :param mode: mode of patch placement
+    :return: the completed image
+    """
+    completed_img = np.zeros(img.shape)
+    if mode == "random":
+
+    return completed_img
